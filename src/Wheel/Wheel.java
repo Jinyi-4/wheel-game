6b88bfaf7d3f4da7a41ee725e3b9876a29c5a2fe
jinyi <2424049321@qq.com>
2025-08-15 20:20:33 +0800

    update

--- src/Wheel/Wheel.java
+++ src/Wheel/Wheel.java
@@ -1,6 +1,8 @@
 package Wheel;
 public class Wheel {
     private static volatile Wheel wheel;
+    private Wheel(){};
+    public static Wheel getWheel(){
         if(wheel ==null){
             synchronized (Wheel.class){
                 if(wheel ==null){
@@ -9,5 +11,10 @@ public class Wheel {
             }
         }
         return wheel;
+    }
+
+    //转盘可以获取奖品，比如衣服，比如消耗品，建模出现了问题
+    public spin{
+
     }
 }
